7677891dad5b0c71f24e1ab878e4fbbe187c9640
Carolina Osuna <carolinaosunardgz@gmail.com>
2018-05-04 23:03:41 +0000

    updated pull logic

--- kappa.py
+++ kappa.py
@@ -3,18 +3,22 @@ import re
 
 conn = psycopg2.connect("dbname=kappa user=postgres")
 cur = conn.cursor()
+
 #take agent from agent on policy details page
 agent = 'Juanita'
 cur.execute("SELECT id FROM crm.employee WHERE first_name = %s", [agent])
 agentid = cur.fetchone()[0]
 print(agentid)
+
 #take carrier from carrier on policy details page
 carrier = 'Geico'
 cur.execute("SELECT id FROM crm.company WHERE name = %s", [carrier])
 carrierid = cur.fetchone()[0]
 print(carrierid)
-dealership = 'Progressive Auto'
-cur.execute("SELECT id FROM crm.company WHERE name = %s", [dealership])
+
+#take dealership from carrier on policy details page
+dealership = 'Momentum Volkswagen of Upper Kirby'
+cur.execute("SELECT id FROM crm.company WHERE name = %s and type = 'dealership'", [dealership])
 dealershipcheck = cur.fetchone()
 if dealershipcheck is None:
     cur.execute("INSERT INTO crm.company (type, name) VALUES ('dealership',%s)", dealership)
@@ -23,6 +27,8 @@ if dealershipcheck is None:
 else:
     dealershipid = dealershipcheck[0]
     print(dealershipid)
+
+#take rep from sales person on policy details page
 rep = 'Shawn Rackley'
 counter = 0
 rep_first_name = ''
@@ -46,6 +52,26 @@ if repcheck is None:
 else:
      repid = repcheck[0]
      print(repid)
-    
+
+#take lienholder from lienholder on policy details page
+lienholder = 'Houston Federal Credit'
+lienphone = 8666874327
+cur.execute("SELECT id, phone FROM crm.company WHERE name = %s and type = 'lienholder'",[lienholder])
+lienholdercheck = cur.fetchone()
+if lienholdercheck is None:
+     cur.execute("INSERT INTO crm.company (type, name, phone) VALUES ('lienholder',%s,%s)", (lienholder,lienphone))
+     conn.commit()
+     print('nope')
+else:
+     lienholderid = lienholdercheck[0]
+     orig_lienphone = lienholdercheck[1]
+     print(lienholderid)
+     print('retrieved')
+     if orig_lienphone != lienphone:
+         cur.execute("UPDATE crm.company SET phone = %s WHERE id = %s", (lienphone,lienholderid))
+         print('updated')
+         conn.commit()   
+
+
 cur.close()
 conn.close()
